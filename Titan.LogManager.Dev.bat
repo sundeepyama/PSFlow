54293e66218f7200935e801201608394de954d8e
Sundeep Yama <SundeepYama@21stMortgage.com>
2019-03-22 12:33:49 -0400

    Added code to publish dev and prod

---
+++ Titan.LogManager.Dev.bat
@@ -0,0 +1,2 @@
+msbuild.exe Titan.LogManagerDev.build
+xcopy "..\Titan.LogManager\bin\debug\app.publish\*.*" "\\sapptrackgamma\TitanDEV\LogManager\" /E /Y 
---
+++ Titan.LogManager.PROD.bat
@@ -0,0 +1,2 @@
+msbuild.exe Titan.LogManagerPROD.build
+xcopy "..\Titan.LogManager\bin\Release\app.publish\*.*" "\\SAppTrackGamma\TitanPROD\LogManager\" /E /Y 
