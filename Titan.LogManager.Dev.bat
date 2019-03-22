244692625aa689c35de666f63bd0a29d953ffa12
Sundeep Yama <SundeepYama@21stMortgage.com>
2019-03-22 12:46:24 -0400

    Changed dev publish file

--- Titan.LogManager.Dev.bat
+++ Titan.LogManager.Dev.bat
@@ -1,2 +1,3 @@
-msbuild.exe Titan.LogManagerDev.build
-xcopy "..\Titan.LogManager\bin\debug\app.publish\*.*" "\\sapptrackgamma\TitanDEV\LogManager\" /E /Y 
+Schema specified is not valid. Errors: 
+EFContexts.Titan.Titan.csdl(1742,6) : error 0040: Type AddressStandardAbbreviation is not defined in namespace TitanModel.
+EFContexts.Titan.Titan.csdl(1780,8) : error 0100: The referenced EntitySet CustomerFileStatusHistories for End CustomerFileStatusHistory could not be found in the containing EntityContainer.
